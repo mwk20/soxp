ef4179bae26ab9fde69587322bfbbf1cab308aad
Matt Kern <matt.kern@cyantechnology.com>
2013-04-25 21:20:08 +0100

    Generated app.

---
+++ src/soxp_app.erl
@@ -0,0 +1,16 @@
+-module(soxp_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    soxp_sup:start_link().
+
+stop(_State) ->
+    ok.
