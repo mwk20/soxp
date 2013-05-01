699bc2719b81a9b2d41e43e2918527ef1d4ec266
Matt Kern <matt.kern@cyantechnology.com>
2013-05-01 21:56:11 +0100

    Readded form of CHILD macro with no arguments.

--- src/soxp_sup.erl
+++ src/soxp_sup.erl
@@ -9,7 +9,8 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
+%% Helper macros for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 -define(CHILD(I, Args, Type), {I, {I, start_link, [Args]}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
