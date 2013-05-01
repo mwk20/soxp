48dc1d789bc4bb9c1b56b52b8f356838589050aa
Matt Kern <matt.kern@cyantechnology.com>
2013-05-01 21:41:57 +0100

    Functional soxp_listener plumbed to example_handler in soxp_sup.

--- src/soxp_listener.erl
+++ src/soxp_listener.erl
@@ -2,11 +2,14 @@
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 
+-record(soxp_listener_state, {lsock,
+                              new_socket_handler}).
+
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -19,26 +22,51 @@
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+start_link(Args) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, Args, []).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
-    {ok, Args}.
+init([{new_socket_handler, Fun}]) when is_function(Fun, 1) ->
+%%init(Args) ->
+    %% ensure terminate is called by supervisor
+    process_flag(trap_exit, true),
+
+    %% create listening socket
+    {ok, LSock} = gen_tcp:listen(8100, [binary, {nodelay, true}, {packet, raw}, {reuseaddr, true}]),
+%% @todo    , {active, true}
+
+    %% we use the gen_server timeout to accept connections
+    {ok, #soxp_listener_state{lsock=LSock, new_socket_handler=Fun}, 0}.
 
 handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
+    %% ignore; timeout required to continue accepting connections
+    {reply, ok, State, 0}.
 
 handle_cast(_Msg, State) ->
-    {noreply, State}.
+    %% ignore; timeout required to continue accepting connections
+    {noreply, State, 0}.
 
+handle_info(timeout, State) ->
+    %% wait for up to 100ms for a new connection
+    %% we wait for a finite time in order to keep servicing otp events
+    case gen_tcp:accept(State#soxp_listener_state.lsock, 100) of
+        {ok, Sock} ->
+            %% pass new connections to new_socket_handler
+            (State#soxp_listener_state.new_socket_handler)(Sock);
+        {error, timeout} ->
+            %% no connection within timeout
+            ok
+    end,
+    {noreply, State, 0};
 handle_info(_Info, State) ->
-    {noreply, State}.
+    %% ignore; timeout required to continue accepting connections
+    {noreply, State, 0}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, State) ->
+    gen_tcp:close(State#soxp_listener_state.lsock),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
--- src/soxp_sup.erl
+++ src/soxp_sup.erl
@@ -10,7 +10,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Args, Type), {I, {I, start_link, [Args]}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -24,5 +24,14 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {ok, { {one_for_one, 5, 10}, [?CHILD(soxp_listener, [{new_socket_handler, fun example_handler/1}], worker)]} }.
 
+example_handler(Sock) ->
+    erlang:spawn(fun() ->
+                         gen_tcp:send(Sock, <<"Foo">>),
+                         timer:sleep(750),
+                         gen_tcp:send(Sock, <<"Bar">>),
+                         timer:sleep(750),
+                         gen_tcp:send(Sock, <<"Baz">>),
+                         gen_tcp:close(Sock)
+                 end).
